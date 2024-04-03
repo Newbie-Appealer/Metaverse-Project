02c85cad739c107e622c024bba79e3522beef41d
Dayo <dmschd0817@naver.com>
2024-04-03 12:01:14 +0900

    플레이어 움직임 구현 中

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,133 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public delegate void PlayControllerDelegate();
+    public PlayControllerDelegate playerController;
+
+    [Header("=== Movement ===")]
+    private Animator _man_Animator;
+    private Rigidbody _rb;
+    private bool _isGrounded = true;
+    private float _moveSpeed = 5f;
+    private float _jumpSpeed = 7f;
+
+    [Header("=== Object ===")]
+    [SerializeField] private GameObject obj_Cam_First, obj_Cam_Quarter;
+    [SerializeField] private GameObject obj_Rotate_Horizontal;
+    [SerializeField] private GameObject obj_Body;
+
+    private PhotonView _pv;
+
+    private void Start()
+    {
+        F_InitController();
+    }
+    public void F_InitController()
+    {
+        _pv = GetComponent<PhotonView>();
+        _rb = GetComponent<Rigidbody>();
+        _man_Animator = GetComponent<Animator>();
+
+        if (_pv.IsMine)
+        {
+            obj_Cam_First.SetActive(false);
+            obj_Cam_Quarter.SetActive(true);
+            this.gameObject.name += "(LocalPlayer)";
+            RankingManager.Instance.F_AddUser(this.gameObject);
+        }
+        else
+        {
+            obj_Cam_First.SetActive(false);
+            obj_Cam_Quarter.SetActive(false);
+            this.gameObject.name += "(OtherPlayer)";
+            RankingManager.Instance.F_AddUser(this.gameObject);
+        }
+
+        F_InitDelegate();
+    }
+
+    public void F_InitDelegate()
+    {
+        if (_pv.IsMine)
+        {
+            playerController += F_PlayerMove;
+        }
+    }
+    private void F_PlayerMove()
+    {
+        float _input_x = Input.GetAxis("Horizontal");
+        float _input_z = Input.GetAxis("Vertical");
+        Vector3 _moveVector;
+         _isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.25f);
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            if (_input_x > 0)
+            {
+                if (_input_z > 0)
+                    obj_Body.transform.localEulerAngles = new Vector3(0f, 45f, 0f);
+                else if (_input_z < 0)
+                    obj_Body.transform.localEulerAngles = new Vector3(0f, 135f, 0f);
+                else
+                    obj_Body.transform.localEulerAngles = new Vector3(0f, 90f, 0f);
+            }
+            else if (_input_x < 0)
+            {
+                if (_input_z > 0)
+                    obj_Body.transform.localEulerAngles = new Vector3(0f, -45f, 0f);
+                else if (_input_z < 0)
+                    obj_Body.transform.localEulerAngles = new Vector3(0f, -135f, 0f);
+                else
+                    obj_Body.transform.localEulerAngles = new Vector3(0f, 270f, 0f);
+            }
+            else
+            {
+                if (_input_z > 0)
+                    obj_Body.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+                else if (_input_z < 0)
+                    obj_Body.transform.localEulerAngles = new Vector3(0f, 180f, 0f);
+            }
+
+            //´Þ¸®±â
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                _man_Animator.SetBool("Walk", false);
+                _man_Animator.SetBool("Run", true);
+                _moveSpeed = 10f;
+            }
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                _man_Animator.SetBool("Run", false);
+                _man_Animator.SetBool("Walk", true);
+                _moveSpeed = 5f;
+            } 
+
+            _moveVector = (transform.right * _input_x + transform.forward * _input_z).normalized;
+            _rb.MovePosition(transform.position + _moveVector * _moveSpeed * Time.deltaTime);
+            _man_Animator.SetBool("Walk", true);
+        }
+        else
+        {
+            _man_Animator.SetBool("Run", false);
+            _man_Animator.SetBool("Walk", false);
+        }
+        if (_isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                F_PlayerJump();
+        }
+    }
+
+    private void F_PlayerJump()
+    {
+        _rb.AddForce(Vector3.up * _jumpSpeed, ForceMode.Impulse);
+        _man_Animator.SetTrigger("Jump");
+    }
+
+    private void Update()
+    {
+        if(_pv.IsMine) 
+            playerController();
+    }
+}
