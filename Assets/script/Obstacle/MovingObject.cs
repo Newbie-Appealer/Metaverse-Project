e9f8db4925fb99edf097072b7bd2d4d0edecaff8
kaffu0424 <kaffu@naver.com>
2024-04-17 10:30:35 +0900

    움직이는 오브젝트 컴포넌트 완료

--- Assets/script/Obstacle/MovingObject.cs
+++ Assets/script/Obstacle/MovingObject.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class MovingObject : MonoBehaviour
 {
+    [Header("local Position")]
     [SerializeField] private Vector3 _targetPosition;
-    private Vector3 _beforePosition;
+    [SerializeField] private Vector3 _beforePosition;
 
+    private bool _isMoving;
     private void Start()
     {
-        _beforePosition = transform.position;
+        _beforePosition = transform.localPosition;
+        _isMoving = false;
     }
 
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (_isMoving)
+            return;
+
         if(collision.gameObject.CompareTag("Player"))
         {
             StartCoroutine(F_MoveObject());
@@ -23,11 +29,12 @@ public class MovingObject : MonoBehaviour
 
     IEnumerator F_MoveObject()
     {
+        _isMoving = true;
         yield return new WaitForSeconds(0.5f);
-        while (Vector3.Distance(transform.position, _targetPosition) > 0.1f)
+        while (Vector3.Distance(transform.localPosition, _targetPosition) > 0.1f)
         {
             yield return new WaitForSeconds(Time.deltaTime);
-            transform.position = Vector3.MoveTowards(transform.position, _targetPosition, 0.1f);
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, _targetPosition, 0.05f);
         }
 
         StartCoroutine(F_ReturnObject());       // 돌아감
@@ -35,11 +42,13 @@ public class MovingObject : MonoBehaviour
 
     IEnumerator F_ReturnObject()
     {
-        yield return new WaitForSeconds(5f);    // 5초 대기후
-        while (Vector3.Distance(transform.position, _beforePosition) > 0.1f)
+        yield return new WaitForSeconds(3f);    // 3초 대기후
+        while (Vector3.Distance(transform.localPosition, _beforePosition) > 0.1f)
         {
             yield return new WaitForSeconds(Time.deltaTime);
-            transform.position = Vector3.MoveTowards(transform.position, _beforePosition, 0.1f);
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, _beforePosition, 0.05f);
         }
+
+        _isMoving = false;
     }
 }
