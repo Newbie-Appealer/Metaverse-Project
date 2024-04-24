313f590d58e4ec105c2af33806091a5332525003
Dayo <dmschd0817@naver.com>
2024-04-24 10:09:01 +0900

    플레이어 정보 못 가져오는 거 수정

--- Assets/Player/PlayerCrashCheck.cs
+++ Assets/Player/PlayerCrashCheck.cs
@@ -6,12 +6,12 @@ public class PlayerCrashCheck : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        GameManager.Instance._players.gameObject.GetComponent<PlayerController>()._isCrashed = true;
-        GameManager.Instance._players.gameObject.GetComponent<PlayerController>().F_GetRB().velocity = Vector3.zero;
+        GameManager.Instance._players.GetChild(0).GetComponent<PlayerController>()._isCrashed = true;
+        GameManager.Instance._players.GetChild(0).GetComponent<PlayerController>().F_GetRB().velocity = Vector3.zero;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        GameManager.Instance._players.gameObject.GetComponent<PlayerController>()._isCrashed = false;
+        GameManager.Instance._players.GetChild(0).GetComponent<PlayerController>()._isCrashed = false;
     }
 }
--- Assets/Player/PlayerGroundCheck.cs
+++ Assets/Player/PlayerGroundCheck.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class PlayerGroundCheck : MonoBehaviour
 {
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        GameManager.Instance._players.gameObject.GetComponent<PlayerController>()._isGrounded = true;
+        GameManager.Instance._players.GetChild(0).GetComponent<PlayerController>()._isGrounded = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        GameManager.Instance._players.gameObject.GetComponent<PlayerController>()._isGrounded = false;
+        GameManager.Instance._players.GetChild(0).GetComponent<PlayerController>()._isGrounded = false;
     }
 }
