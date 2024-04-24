ac325447246286a96177b8a1770dd2b85cba5748
Da Hoon <binanuku@gmail.com>
2024-04-24 10:34:33 +0900

    d

--- Assets/script/Obstacle/FallObject.cs
+++ Assets/script/Obstacle/FallObject.cs
@@ -4,27 +4,11 @@ using UnityEngine;
 
 public class FallObject : MonoBehaviour
 {
-    Vector3 currentPosition;
-    Quaternion currentRotation;
-    Rigidbody rb;
-    private void Start()
-    {
-        currentPosition = transform.position;
-        currentRotation = transform.rotation;
-    }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            rb = gameObject.AddComponent<Rigidbody>();
+            gameObject.AddComponent<Rigidbody>();
         }
-        StartCoroutine(F_ReturnObject());
-    }
-    IEnumerator F_ReturnObject()
-    {
-        yield return new WaitForSeconds(3f);   // 3초 대기후
-        transform.position = currentPosition;
-        transform.rotation = currentRotation;
-        Destroy(rb);
     }
 }
