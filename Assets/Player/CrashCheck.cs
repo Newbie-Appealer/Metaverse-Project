201bf3b15a62e393efa30293080d7f227c9db6bd
kaffu0424 <kaffu@naver.com>
2024-06-19 10:57:06 +0900

    되돌리기

--- Assets/Player/CrashCheck.cs
+++ Assets/Player/CrashCheck.cs
@@ -7,16 +7,6 @@ public class CrashCheck : MonoBehaviour
     [SerializeField] private PlayerController _playerCtr;
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                other.GetComponent<Rigidbody>().AddForce(Vector3.forward * 10f, ForceMode.Impulse);
-            }
-
-            return;
-        }
-
         if (_playerCtr != null && !_playerCtr._isCrashed)
         {
             _playerCtr._isCrashed = true;
